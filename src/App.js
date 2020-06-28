2cd55af47bdce69a169ddd2592764285ea44c2f1
chamod-gamage <chamodgamage26@gmail.com>
2020-06-28 11:24:10 -0400

    react-ga

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "./components/Header";
 import RecipeList from "./components/RecipeList";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,8 +6,15 @@ import "@shopify/polaris/styles.css";
 import "./index.css";
 import RecipeContextProvider from "./contexts/RecipeContext";
 import RecipeForm from "./components/RecipeForm";
+import ReactGA from 'react-ga';
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.initialize('UA-170137058-1');
+    ReactGA.pageview('/homepage');
+  }, [])
+
   return (
     <div className="App">
       <div className="container mt-5">
